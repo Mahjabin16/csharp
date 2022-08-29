b9e324e73feb55410ad2a196ffac7db92de7d587
Mahjabin16 <85182027+Mahjabin16@users.noreply.github.com>
2022-08-30 05:02:44 +0600

    Add files via upload

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.devide = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.star = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.SystemColors.Highlight;
+            this.seven.Location = new System.Drawing.Point(12, 117);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(74, 53);
+            this.seven.TabIndex = 0;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = false;
+            // 
+            // eight
+            // 
+            this.eight.BackColor = System.Drawing.SystemColors.Highlight;
+            this.eight.Location = new System.Drawing.Point(92, 117);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(80, 53);
+            this.eight.TabIndex = 1;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = false;
+            // 
+            // nine
+            // 
+            this.nine.BackColor = System.Drawing.SystemColors.Highlight;
+            this.nine.Location = new System.Drawing.Point(178, 117);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(79, 53);
+            this.nine.TabIndex = 2;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = false;
+            // 
+            // devide
+            // 
+            this.devide.BackColor = System.Drawing.SystemColors.Highlight;
+            this.devide.Location = new System.Drawing.Point(277, 117);
+            this.devide.Name = "devide";
+            this.devide.Size = new System.Drawing.Size(79, 53);
+            this.devide.TabIndex = 4;
+            this.devide.Text = "/";
+            this.devide.UseVisualStyleBackColor = false;
+            // 
+            // four
+            // 
+            this.four.BackColor = System.Drawing.SystemColors.Highlight;
+            this.four.Location = new System.Drawing.Point(11, 176);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(75, 54);
+            this.four.TabIndex = 6;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = false;
+            // 
+            // five
+            // 
+            this.five.BackColor = System.Drawing.SystemColors.Highlight;
+            this.five.Location = new System.Drawing.Point(93, 176);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(79, 54);
+            this.five.TabIndex = 8;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = false;
+            // 
+            // six
+            // 
+            this.six.BackColor = System.Drawing.SystemColors.Highlight;
+            this.six.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.six.Location = new System.Drawing.Point(178, 176);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(79, 54);
+            this.six.TabIndex = 9;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = false;
+            // 
+            // star
+            // 
+            this.star.BackColor = System.Drawing.SystemColors.Highlight;
+            this.star.Location = new System.Drawing.Point(277, 176);
+            this.star.Name = "star";
+            this.star.Size = new System.Drawing.Size(79, 54);
+            this.star.TabIndex = 10;
+            this.star.Text = "*";
+            this.star.UseVisualStyleBackColor = false;
+            // 
+            // one
+            // 
+            this.one.BackColor = System.Drawing.SystemColors.Highlight;
+            this.one.Location = new System.Drawing.Point(11, 236);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(74, 54);
+            this.one.TabIndex = 11;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = false;
+            // 
+            // two
+            // 
+            this.two.BackColor = System.Drawing.SystemColors.Highlight;
+            this.two.Location = new System.Drawing.Point(91, 236);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(79, 54);
+            this.two.TabIndex = 12;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = false;
+            // 
+            // three
+            // 
+            this.three.BackColor = System.Drawing.SystemColors.Highlight;
+            this.three.Location = new System.Drawing.Point(178, 236);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(79, 54);
+            this.three.TabIndex = 13;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = false;
+            // 
+            // plus
+            // 
+            this.plus.BackColor = System.Drawing.SystemColors.Highlight;
+            this.plus.Location = new System.Drawing.Point(277, 236);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(79, 54);
+            this.plus.TabIndex = 14;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = false;
+            // 
+            // minus
+            // 
+            this.minus.BackColor = System.Drawing.SystemColors.Highlight;
+            this.minus.Location = new System.Drawing.Point(277, 296);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(79, 54);
+            this.minus.TabIndex = 15;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = false;
+            // 
+            // zero
+            // 
+            this.zero.BackColor = System.Drawing.SystemColors.Highlight;
+            this.zero.Location = new System.Drawing.Point(10, 296);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(75, 54);
+            this.zero.TabIndex = 16;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = false;
+            // 
+            // dot
+            // 
+            this.dot.BackColor = System.Drawing.SystemColors.Highlight;
+            this.dot.Location = new System.Drawing.Point(90, 296);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(80, 54);
+            this.dot.TabIndex = 17;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = false;
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.clear.Location = new System.Drawing.Point(178, 296);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(79, 54);
+            this.clear.TabIndex = 18;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = false;
+            // 
+            // equals
+            // 
+            this.equals.BackColor = System.Drawing.SystemColors.Highlight;
+            this.equals.Location = new System.Drawing.Point(10, 356);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(346, 53);
+            this.equals.TabIndex = 19;
+            this.equals.Text = "=";
+            this.equals.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.AlphaFull;
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(344, 68);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(368, 418);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.star);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.devide);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button devide;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button star;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
